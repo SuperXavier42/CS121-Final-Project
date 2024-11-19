80bc572b5b69d16a8c2311815fd9dc2efb5439bb
Xavier <superxavier100@gmail.com>
2024-11-19 15:11:06 -0600

    added comments to code

--- main.cpp
+++ main.cpp
@@ -19,38 +19,41 @@ class Calculator : public Computer{
         string name;
         double current_val=0;
     public:
+        //sets name for calculator I guess
         void setName(string name){this->name=name;}
+
+        //allows user to calculate a number using basic opperations
         int calculate(){
             cout<<"Enter = for opperation to see result\n";
             cout<<"Enter number: ";
+            //sets current val to the initial number
             cin>>this->current_val;
 
             char opp;
             cout<<"Enter operation: ";
             cin>>opp;
 
+            //checks if the first value is a =
             if(opp=='='){cout<<"Final value: "<<this->current_val<<endl; return this->current_val;}
 
+            //runs until user enters a =
             while(true){
                 double temp;
                 cout<<"Enter number: ";
                 cin>>temp;
 
+                //updates current val based on entered opperation
                 switch (opp){
                     case '+':
-                        //cout<<"+\n";
                         this->current_val=this->current_val+temp;
                         break;
                     case '-':
-                        //cout<<"-\n";
                         this->current_val=this->current_val-temp;
                         break;
                     case '/':
-                        //cout<<"/\n";
                         this->current_val=this->current_val/temp;
                         break;
                     case '*':
-                        //cout<<"*\n";
                         this->current_val=this->current_val*temp;
                         break;
                     case '=':
@@ -64,6 +67,7 @@ class Calculator : public Computer{
 
                 if(opp=='=')break;
             }
+            
             cout<<"Final value: "<<this->current_val<<endl;
             return this->current_val;
         };
